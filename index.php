38ed7ac2e76f7af362d69dfe6c1baa887b7dc9f2
Davi Teixeira <86988184+dkat-davi@users.noreply.github.com>
2023-10-23 09:03:19 -0300

    feat: definindo a estrutura de pastas

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+</head>
+<body>
+    <h1>Página Home</h1>
+    <ul>
+        <li><a href="pages/gerenciar">Gerenciar</a></li>
+        <li><a href="pages/caixa">Caixa</a></li>
+        <li><a href="pages/perfil">perfil</a></li>
+    </ul>
+    <hr>
+    <h2>Produtos</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis illo neque, aliquid error non magnam.</p>
+    <p>Odit, magnam ea vero recusandae rem error velit dignissimos, perferendis iste sed veritatis earum harum!</p>
+    <p>Hic exercitationem vel repudiandae, nam consectetur asperiores vero, explicabo voluptatibus unde quod officiis voluptate fugit!</p>
+    <p>Repellendus quam qui, similique voluptatem eos ut modi id distinctio quia vel sapiente, delectus recusandae?</p>
+    <p>Possimus quam error mollitia nostrum maxime amet in vel suscipit consequatur sequi beatae, eligendi neque!</p>
+</body>
+</html>
---
+++ pages/gerenciar/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href=""></a>Criar Usuário</li>
+        <li><a href=""></a>Listar Usuários</li>
+        <li><a href=""></a>Deletar Usuários</li>
+        <li><a href=""></a>Editar Usuários</li>
+    </ul>
+</body>
+</html>
