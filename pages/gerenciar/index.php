0029a7676451d0b1244f346b2fe27ac37bc417fe
jbragas618 <130567926+jbragas618@users.noreply.github.com>
2023-10-25 11:06:07 -0300

    Feat:Adicionei h1 nas páginas d gerenciar

--- pages/gerenciar/index.php
+++ pages/gerenciar/index.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Página do Gerente</title>
 </head>
+    <header>
+        <h1> Página do Gerente</h1>
+    </header>
 <body>
     <ul>
-        <li><a href=""></a>Criar Usuário</li>
-        <li><a href=""></a>Listar Usuários</li>
-        <li><a href=""></a>Deletar Usuários</li>
-        <li><a href=""></a>Editar Usuários</li>
+        <li><a href="../usuarios/create.php">Criar Usuário</a></li>
+        <li><a href="../usuarios/all.php">Listar Usuários</a></li>
+        <li><a href="../usuarios/delete.php">Deletar Usuários</a></li>
+        <li><a href="../usuarios/update.php">Editar Usuários</a></li>
     </ul>
 </body>
 </html>
