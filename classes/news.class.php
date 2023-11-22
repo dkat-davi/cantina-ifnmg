6d3e0c6747068ff4050830ba0383fdf9fcf50cf1
jbragas618 <130567926+jbragas618@users.noreply.github.com>
2023-11-22 10:50:10 -0300

    pagina de criação de noticias e news.class

---
+++ classes/news.class.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'r.class.php';
+require_once 'database.class.php';
+
+
+class News
+{
+   
+public static function Create()
+{
+    DB::Start();
+
+   
+
+
+}
+
+
+
+
+   
+   
+
+       
+}
+
+
+?>
---
+++ pages/news/create.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+</head>
+<body>
+<header>Create News</header>   
+<main>
+    <form>
+        <fieldset>
+            <label for='titulo'>Titulo</label>
+            <input type='text' id='titulo' name='titulo'><br><br>
+
+            <label for='descricao'>Descricao</label>
+            <textarea id='descricao' name='descricao' rows='4' cols='50'></textarea><br><br>    
+
+            <label for='autor'>Autor</label>
+            <input type='text' id='autor' name='autor'><br><br>  
+
+            <label for='imagem'>Imagem</label>
+            <input type="file" name="imagem" accept="image/png" required>
+
+
+
+        </fieldset>
+    </form>
+
+</main>
+</html>
