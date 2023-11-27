4601fb12761cdafc6e566d5fb925de735f21b20d
jbragas618 <130567926+jbragas618@users.noreply.github.com>
2023-11-27 08:50:10 -0300

    método implementado para salvamento no BD

--- classes/news.class.php
+++ classes/news.class.php
@@ -7,10 +7,22 @@ require_once 'database.class.php';
 class News
 {
    
-public static function Create()
+public static function Create($titulo , $descricao , $autor , $imagem)
 {
-    DB::Start();
+   
+        DB::Start();
 
+        $news = R::dispense('news');
+
+        $news->titulo = $titulo;
+        $news->descricao = $descricao;
+        $news->autor = $autor;
+        $news->imagem = $imagem;
+
+        R::store($news);
+        R::close();
+
+    
    
 
 
